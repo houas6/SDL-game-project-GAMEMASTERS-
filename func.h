261c7d1bb30118e137ed87f3bb1c09986e5a3b93
amin0123 <lamloumamin01@gmail.com>
2021-03-07 02:52:58 -0800

    ajout func.h

---
+++ func.h
@@ -0,0 +1,12 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL_mixer.h>
+void setrect(SDL_Rect* clip);
+void credit_menu(SDL_Surface *screen_c,int etat);
+void option_menu(SDL_Surface *screen_op,int *v,int *etat); 
+void menu();
+
+
